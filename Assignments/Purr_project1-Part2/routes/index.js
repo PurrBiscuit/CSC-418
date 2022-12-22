335315acd61a1df08512d3860e1671bfd8c277c0
Purrbiscuit <spurr@articulate.com>
2022-12-22 14:26:57 -0500

    add navbar to layout.ejs

--- Assignments/Purr_project1-Part2/routes/index.js
+++ Assignments/Purr_project1-Part2/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Express' })
 })
 
+router.get('/add-contact', (req, res) => {
+  res.render('addContactForm')
+})
+
 module.exports = router
--- Assignments/Purr_project1-Part2/routes/users.js
+++ Assignments/Purr_project1-Part2/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.send('respond with a resource')
+  res.render('users')
 })
 
 module.exports = router
