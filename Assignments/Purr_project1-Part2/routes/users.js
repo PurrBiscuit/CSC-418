af88098a8189cefeee0600da4678cd1978fa04a4
Purrbiscuit <spurr@articulate.com>
2022-12-22 17:53:12 -0500

    add contact us form to part 2 of project 1

--- Assignments/Purr_project1-Part2/routes/users.js
+++ Assignments/Purr_project1-Part2/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const users = require('../data/users')
+
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.render('users')
+  res.render('users', { users })
 })
 
 module.exports = router
