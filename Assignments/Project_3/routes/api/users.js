ce0e97d9d45ca6eb8d1483d6128ff196250b7a12
Purrbiscuit <spurr@articulate.com>
2023-01-16 15:55:53 -0500

    add middleware to check for format=json query string

--- Assignments/Project_3/routes/api/users.js
+++ Assignments/Project_3/routes/api/users.js
@@ -4,6 +4,18 @@ const router = express.Router()
 
 const contacts = require('../../lib/model/contact')
 
+const contentTypeCheck = (req, res, next) => {
+  if (!req.query || !req.query.format)
+    return res.status(400).send('Please include the format query string in your request.')
+
+  if (req.query.format !== 'json')
+    return res.status(400).send('Format not supported - please use json.')
+
+  next()
+}
+
+router.use(contentTypeCheck)
+
 // return all users
 router.get('/', (req, res) => {
   contacts.find().lean()
@@ -15,8 +27,16 @@ router.get('/', (req, res) => {
 // return a single user based on id
 router.get('/:userId', (req, res) => {
   contacts.findById(req.params.userId).lean()
-    .then(({ gender, id, name }) => {
-      res.json({ gender, id, name })
+    .then((record) => {
+      if (!record) {
+        const error = createError(404)
+        return res.status(error.status).send(error)
+      }
+
+      res.json(record)
+    })
+    .catch(error => {
+      res.status(500).send(createError(error.message))
     })
 })
 
